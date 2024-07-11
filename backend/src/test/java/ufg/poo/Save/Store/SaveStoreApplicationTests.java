2fb89e257e59b088fbdc22639122990fd2f80086
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-11 16:44:39 -0300

    theusin fez um commit

--- backend/src/test/java/ufg/poo/Save/Store/SaveStoreApplicationTests.java
+++ backend/src/test/java/ufg/poo/Save/Store/SaveStoreApplicationTests.java
@@ -3,6 +3,7 @@ package ufg.poo.Save.Store;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// comentario do theusin alksalksalks
 @SpringBootTest
 class SaveStoreApplicationTests {
 
