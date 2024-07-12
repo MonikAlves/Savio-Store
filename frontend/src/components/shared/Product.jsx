45d58a0fdfe9bfa3bf4d3239798a6d59756da441
manotv-alt <manotvloco0@gmail.com>
2024-07-11 22:23:38 -0300

    att

--- frontend/src/components/shared/Product.jsx
+++ frontend/src/components/shared/Product.jsx
@@ -1,13 +1,22 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ShoppingContext } from "../../contexts/ShoppingProvider";
 
 export function Product({image, title, description, price}){
 
-    //const {addToCart} = useContext(ShoppingContext); 
+    const {addToCart} = useContext(ShoppingContext); 
 
-    const changeColor = () => {
-        setColor((prevColor) => (prevColor === 'bg-white/100' ? 'bg-red-700' : 'bg-white/100'));
-      };
+    const [clicked, setClicked] = useState(false);
+    const [buttonState, setButtonState] = useState(false);
+
+    const currentClicked = () => {
+        setClicked(!clicked);
+        setButtonState(clicked ? false : true);
+    }
+
+    const handleClick = () => {
+        setClicked(!clicked);
+        setButtonState(clicked ? false : true);
+    };
 
     return (
         <div className="w-80 h-120 bg-gray-700 text-white aspect-square flex flex-col items-center p-2.5 ring-1 ring-white rounded">
@@ -27,13 +36,15 @@ export function Product({image, title, description, price}){
             </div>
             <div className="p-5 flex gap-4 flex-wrap flex-col items-center">
                 <div className="flex gap-2 space-x-3">
-                    <button className="w-9 text-black font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all" onClick={changeColor}>S</button>
-                    <button className="w-9 text-black font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all">M</button>
-                    <button className="w-9 text-black font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all">L</button>
+                    <button className={`w-9 text-black font-bold rounded p-2 cursor-pointer text-center hover:bg-orange-500 transition-all ${clicked ? 'bg-orange-600 hover:bg-orange-600' : 'bg-white'}`} onClick={() => {setClicked(!clicked, "P"); handleClick();}}>S</button>
+                    <button className={`w-9 text-black font-bold rounded p-2 cursor-pointer text-center hover:bg-orange-500 transition-all ${clicked ? 'bg-orange-600 hover:bg-orange-600' : 'bg-white'}`} onClick={() => {setClicked(!clicked, "M"); handleClick();}}>M</button>
+                    <button className={`w-9 text-black font-bold rounded p-2 cursor-pointer text-center hover:bg-orange-500 transition-all ${clicked ? 'bg-orange-600 hover:bg-orange-600' : 'bg-white'}`} onClick={() => {setClicked(!clicked), "G"; handleClick();}}>L</button>
                 </div>
-                <button className="text-black w-full max-w-96 font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all">Comprar</button>
-                <button className="text-black w-full font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all" /*onClick={() => addToCart(product)}*/> Adicionar ao carrinho </button>
+                <button className="text-black w-full max-w-96 font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-orange-600 transition-all">Comprar</button>
+                <button className="text-black w-full font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-orange-600 transition-all" /*onClick={() => addToCart(product)}*/> Adicionar ao carrinho </button>
             </div>
         </div>
     )
 }
+
+//`w-9 text-black font-bold bg-white/100 rounded p-2 cursor-pointer text-center hover:bg-red-700 hover:text-white transition-all"
