b18c3856d2a3b51bbd61eacea8fd41672c448cdf
LuisFcamo <luis1170da@gmail.com>
2024-07-23 15:40:14 -0300

    aaaa

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,6 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {
       colors: {
@@ -25,8 +24,6 @@ export default {
       luizImage: {
         'masc-roupas': "url('/luiz.png')",
       },
-
-      
 
     },
   },
