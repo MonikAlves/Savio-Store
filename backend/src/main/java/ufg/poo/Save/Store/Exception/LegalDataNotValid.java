68fb288a185bc7d29a9916ec6b5205244714a4b1
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-23 22:49:01 -0300

    correção no arquivo

--- backend/src/main/java/ufg/poo/Save/Store/Exception/LegalDataNotValid.java
+++ backend/src/main/java/ufg/poo/Save/Store/Exception/LegalDataNotValid.java
@@ -1,12 +1,7 @@
-
-
 package ufg.poo.Save.Store.Exception;
 
 import org.springframework.http.HttpStatus;
 
-/**
- * LegalDataNotValid
- */
 public class LegalDataNotValid extends SuperException {
     public LegalDataNotValid(String message) {
         // TODO
