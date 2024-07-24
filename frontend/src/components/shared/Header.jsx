a0b533e391d41418312c1529bfbf6f0d79331ad1
manotv-alt <manotvloco0@gmail.com>
2024-07-23 21:43:37 -0300

    nd demais

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -75,7 +75,6 @@ export function Header() {
                             <LogIn />
                             Logout
                         </button>
-
                     </div>
                     )}
 
