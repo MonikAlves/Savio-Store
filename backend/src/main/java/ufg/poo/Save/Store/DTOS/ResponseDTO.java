3ca56385fe7d7062efa66495d6abbc053346cb0c
Monik <monikcostaal@gmail.com>
2024-07-24 16:20:19 -0300

    documentação de dtos criada

--- backend/src/main/java/ufg/poo/Save/Store/DTOS/ResponseDTO.java
+++ backend/src/main/java/ufg/poo/Save/Store/DTOS/ResponseDTO.java
@@ -4,6 +4,11 @@ import org.springframework.http.ResponseEntity;
 import ufg.poo.Save.Store.Exception.SuperException;
 
 public class ResponseDTO {
+    /**
+     * @brief Generate an error from exception
+     * @param e Exception to be converted to na error
+     * @return A response entity to error
+     */
     public static ResponseEntity<ErrorDTO> response(SuperException e) {
         ErrorDTO error = new ErrorDTO();
         error.setError(e.getMessage());
