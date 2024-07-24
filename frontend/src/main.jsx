e1f6a351b51f75a8818e7d55c8048bed5dba175c
manotv-alt <manotvloco0@gmail.com>
2024-07-24 15:15:46 -0300

    att

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,10 +9,7 @@ import { ContactMain } from './components/modules/ContactMain.jsx';
 import { LoginMain } from './components/modules/LoginMain.jsx';
 import { RegisterMain } from './components/modules/RegisterMain.jsx';
 import { CartMain } from './components/modules/CartMain.jsx';
-<<<<<<< HEAD
 import { InfoProduct } from './components/modules/InfoProduct.jsx';
-=======
->>>>>>> e19ed83de4b40e80f57e07f67e5b12ef1563c95a
 
 const router = createBrowserRouter([
   {
