77b4e7689aa5a6be5f92c0d37f476fb2e5c7b841
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-26 02:36:30 -0300

    documentação da exceção BadRequest criada

--- backend/src/main/java/ufg/poo/Save/Store/Exception/BadRequestException.java
+++ backend/src/main/java/ufg/poo/Save/Store/Exception/BadRequestException.java
@@ -2,7 +2,16 @@ package ufg.poo.Save.Store.Exception;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Exception to Bad Request
+ *
+ */
 public class BadRequestException extends SuperException {
+    /**
+     * Constructor to Bad Request
+     *
+     * @param message Message exception
+     */
     public BadRequestException(String message) {
         super(message, HttpStatus.BAD_REQUEST);
     }
