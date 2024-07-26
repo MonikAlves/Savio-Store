18a75bb3b93733a36b2af4b58c34e7db3fe37de1
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-26 02:58:48 -0300

    documentação de cart entity criada

--- backend/src/main/java/ufg/poo/Save/Store/Entities/Cart.java
+++ backend/src/main/java/ufg/poo/Save/Store/Entities/Cart.java
@@ -5,6 +5,10 @@ import lombok.*;
 
 import java.io.Serializable;
 
+/**
+ * Cart Entity
+ *
+ */
 @Entity
 @Table(name="cart")
 @Getter
@@ -13,27 +17,48 @@ import java.io.Serializable;
 @NoArgsConstructor
 @ToString
 public class Cart implements Serializable {
+    /**
+     * The indentifier of the cart
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    /**
+     * The product of the cart
+     */
     @ManyToOne
     @JoinColumn(name = "id_product", referencedColumnName = "id", nullable = false)
     private Product product;
 
+    /**
+     * The client of the cart
+     */
     @ManyToOne
     @JoinColumn(name = "id_client", referencedColumnName = "id", nullable = false)
     private Client client;
 
+    /**
+     * The quantity of the product
+     */
     @Column(nullable = false)
     private int quantity;
 
+    /**
+     * The size of the product
+     */
     @Column(nullable = false)
     private String size;
 
+    /**
+     * The availability of the cart procut
+     */
     @Column(nullable = false)
     private boolean available;
 
+    /**
+     * The total value of the cart
+     */
     @Column(nullable = false)
     private double total;
 }
